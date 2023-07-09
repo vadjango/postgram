a1e5cb6be96ce8f8749a2ff3a1859e5109dcb52b
vadjango <z9661744@gmail.com>
2023-07-09 11:51:47 +0300

    Configured server

---
+++ postgram/settings/dev.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = True
+
+DATABASES = {
+    # FOR DEVELOPMENT
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("NAME"),
+        'USER': os.getenv("USER"),
+        'PASSWORD': os.getenv("PASSWORD"),
+        'HOST': os.getenv("HOST"),
+        'PORT': os.getenv("PORT")
+    }
+}
